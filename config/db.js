95c7870967a6d825737920d331ecb0c44be9f79f
bathulamamatha03@gmail.com <bathulamamatha03@gmail.com>
2026-06-13 18:02:20 +0530

    Update database configuration

--- config/db.js
+++ config/db.js
@@ -1,22 +1,23 @@
 const mysql = require("mysql2");
 
-// Create MySQL Connection
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-// Connect to MySQL
 connection.connect((err) => {
-    if (err) {
-        console.log(" Database connection failed:");
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log("Database connection failed:");
+    console.log(err);
+    return;
+  }
 
-    console.log(" MySQL Connected Successfully");
+  console.log("MySQL Connected Successfully");
 });
 
 module.exports = connection;
+
+nJSOfnDTRdbNPXUazemUHBRuFzvCZnEv
